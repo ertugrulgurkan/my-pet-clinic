6479fcf8d6d47daa1349b32246e4280b4c034bcd
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-06-22 20:17:22 +0300

    Initial commit

---
+++ src/main/java/com/ertugrul/mypetclinic/MyPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.ertugrul.mypetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPetClinicApplication.class, args);
+    }
+
+}
