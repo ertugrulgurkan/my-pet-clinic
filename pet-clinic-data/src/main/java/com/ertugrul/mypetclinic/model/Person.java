eec6b300db9a6cb5f31dc7670d11165819a89395
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-06-23 20:43:18 +0300

    adding base entity class for model

--- pet-clinic-data/src/main/java/com/ertugrul/mypetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/ertugrul/mypetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.ertugrul.mypetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/com/ertugrul/mypetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/ertugrul/mypetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.ertugrul.mypetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
