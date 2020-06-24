1cea50a308b7a335179d38ff4200606cce231765
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-06-24 10:31:19 +0300

    Impl map based service.

--- pet-clinic-data/src/main/java/com/ertugrul/mypetclinic/services/CrudService.java
+++ pet-clinic-data/src/main/java/com/ertugrul/mypetclinic/services/CrudService.java
@@ -6,7 +6,7 @@ public interface CrudService <T,ID>{
 
     Set<T> findAll();
 
-    T findById();
+    T findById(ID id);
 
     T save(T object);
 
---
+++ pet-clinic-data/src/main/java/com/ertugrul/mypetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package com.ertugrul.mypetclinic.services.map;
+
+import com.ertugrul.mypetclinic.model.Owner;
+import com.ertugrul.mypetclinic.model.Vet;
+import com.ertugrul.mypetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet,Long> implements CrudService<Vet,Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
