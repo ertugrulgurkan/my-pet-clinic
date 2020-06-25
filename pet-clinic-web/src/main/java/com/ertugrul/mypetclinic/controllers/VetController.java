fe60cd75bd330511763fed74a5d8381802adbb96
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-06-25 13:38:20 +0300

    added vetList and updated controller.

--- pet-clinic-web/src/main/java/com/ertugrul/mypetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/ertugrul/mypetclinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package com.ertugrul.mypetclinic.controllers;
 
+import com.ertugrul.mypetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "vets/index", "vets/index.html"})
-    public String ListsVets() {
+    public String ListsVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
