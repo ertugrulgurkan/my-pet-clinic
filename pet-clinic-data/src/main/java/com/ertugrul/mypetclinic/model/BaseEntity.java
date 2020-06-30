2fc70668b515712bd0fefa6bc553d7e93c1c4aec
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-06-30 09:00:18 +0300

    Base Entity with Jpa annotation.

--- pet-clinic-data/src/main/java/com/ertugrul/mypetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/ertugrul/mypetclinic/model/BaseEntity.java
@@ -1,10 +1,18 @@
 package com.ertugrul.mypetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
     private String name;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public String getName() {
