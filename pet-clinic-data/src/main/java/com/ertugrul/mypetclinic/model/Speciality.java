96190df1fcf0e6a1bf1e35d7956c7a24f5d22a1b
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-06-30 09:51:58 +0300

    updated vet with jpa

--- pet-clinic-data/src/main/java/com/ertugrul/mypetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/ertugrul/mypetclinic/model/Speciality.java
@@ -1,7 +1,15 @@
 package com.ertugrul.mypetclinic.model;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity{
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
