d1f8733a90a38b367a45f9d817f5ec993427cddd
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-06-30 09:24:42 +0300

    updated owner with jpa

--- pet-clinic-data/src/main/java/com/ertugrul/mypetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/ertugrul/mypetclinic/model/Pet.java
@@ -1,11 +1,21 @@
 package com.ertugrul.mypetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public PetType getPetType() {
