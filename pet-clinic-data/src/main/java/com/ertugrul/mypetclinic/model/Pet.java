738c0c654c0b98f122e6cbe2896df37d76a6f76c
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-06-30 10:04:43 +0300

    added visit entity

--- pet-clinic-data/src/main/java/com/ertugrul/mypetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/ertugrul/mypetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package com.ertugrul.mypetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -17,6 +19,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public PetType getPetType() {
         return petType;
--- pet-clinic-data/src/main/java/com/ertugrul/mypetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/ertugrul/mypetclinic/model/Visit.java
@@ -1,16 +1,24 @@
 package com.ertugrul.mypetclinic.model;
 
-import com.sun.xml.internal.rngom.parse.host.Base;
-
+import javax.persistence.*;
 import java.time.LocalDate;
 
-public class Visit extends Base {
+@Entity
+@Table(name = "visits")
+public class Visit extends BaseEntity {
+
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
-        return date;
+        return  date;
     }
 
     public void setDate(LocalDate date) {
